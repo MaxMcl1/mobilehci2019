0723ef240f9fe259f38645be19a7e9fb4084896b
olliegardner <olliegardner@gmail.com>
2020-02-27 23:03:46 +0000

    map now has markers

--- CampusTour/app/src/main/java/com/mobilehci/campustour/ui/home/HomeFragment.java
+++ CampusTour/app/src/main/java/com/mobilehci/campustour/ui/home/HomeFragment.java
@@ -3,10 +3,13 @@ package com.mobilehci.campustour.ui.home;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.mobilehci.campustour.Building;
 
 public class HomeFragment extends SupportMapFragment implements OnMapReadyCallback {
 
-    GoogleMap mGoogleMap;
+    private GoogleMap mMap;
 
     @Override
     public void onResume() {
@@ -16,17 +19,20 @@ public class HomeFragment extends SupportMapFragment implements OnMapReadyCallba
     }
 
     private void setUpMapIfNeeded() {
-
-        if (mGoogleMap == null) {
+        if (mMap == null) {
             getMapAsync(this);
         }
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap)
-    {
-        mGoogleMap=googleMap;
-        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        for (Building building : Building.values()) {
+            if (building.isVisited()) {
+                mMap.addMarker(new MarkerOptions().position(new LatLng(building.getLatitude(), building.getLongitude())).title(building.getName()));
+            }
+        }
     }
 
     /*private HomeViewModel homeViewModel;
