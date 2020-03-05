b9be3bc18a6a94f8f779d426b71d9e5648acacc3
olliegardner <olliegardner@gmail.com>
2020-03-05 12:52:21 +0000

    added all/my buildings pages

--- CampusTour/app/src/main/java/com/mobilehci/campustour/ui/my/MyFragment.java
+++ CampusTour/app/src/main/java/com/mobilehci/campustour/ui/my/MyFragment.java
@@ -4,32 +4,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
+import com.mobilehci.campustour.Building;
 import com.mobilehci.campustour.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyFragment extends Fragment {
 
-    private MyViewModel myViewModel;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        myViewModel =
-                ViewModelProviders.of(this).get(MyViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_my, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        myViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        List<String> buildings = new ArrayList<>();
+
+        for (Building b : Building.values()) {
+            if (b.isVisited()) buildings.add(b.getName());
+        }
+
+        ArrayAdapter<String> myBuildingsAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, buildings);
+
+        ListView listView = root.findViewById(R.id.list_my_buildings);
+        listView.setAdapter(myBuildingsAdapter);
+
+        TextView emptyText = root.findViewById(R.id.empty_text);
+        listView.setEmptyView(emptyText);
+
         return root;
     }
 }
